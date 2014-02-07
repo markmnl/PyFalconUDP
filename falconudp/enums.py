ae1c25f08ce80e2c1c0c14fcb14062bcac71bceb
Mark Mennell <markmnl@outlook.com>
2014-02-07 13:45:07 +0800

    Added enums.py with LogLevel class, moved stuff around and added _log function to FalconPeer

---
+++ falconudp/enums.py
@@ -0,0 +1,12 @@
+class LogLevel():
+    """Severity of a log entry."""
+
+    All         = 1
+    Debug       = 2
+    Info        = 3
+    Warning     = 4
+    Error       = 5
+    Fatal       = 6
+    NoLogging   = 7
+
+
--- falconudp/falconpeer.py
+++ falconudp/falconpeer.py
@@ -1,28 +1,52 @@
 import socket
+import datetime
+from falconudp.enums import LogLevel
 
-class FalconPeer(object):
+class FalconPeer():
+    """Represents a FalconUDP peer which can discover, join and communicate 
+       with other compatible FalconUDP peers reachable on the same network. """
+
+    def __init__(self, port=37896, log_level=LogLevel.Info):
+        self._port = port
+        self._log_level = log_level
+
+    def _log(self, log_level, msg):
+        if(log_level >= self._log_level):
+            line = '{0:%Y-%m-%d %H:%M:%S}\t{1}\t{2}\t{3}'.format(datetime.datetime.now(), 
+                                                        self._port,
+                                                        log_level, 
+                                                        msg)
+            print(line) # TODO log instead
+
+
+    def _check_started(self):
+        if not hasattr(self, '_started') or not self._started:
+            raise Exception('not started')
+
+    def _process_data(self, data):
+        pass
+    
+    def _recv(self):
+        self.check_started()
+        (data, addr) = self._socket.recvfrom(1024)
+        process_data(data)
 
     def start(self):
+        """TODO"""
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind(('', self._port))
         sock.listen(0) # presumably this is irrelevant for dgram sockets?
         self._socket = sock
         self._started = True
+        _log(LogLevel.Info, 'Started, listening on port: {0}.'.format(_port))
 
-    def check_started(self):
-        if not hasattr(self, '_started') or not self._started:
-            raise Exception('not started')
+    def stop(self):
+        """TODO"""
+        _check_started()
+        # TODO
+        self._started = False
+        _log(LogLevel.Info, 'Stopped.')
 
-    def process_data(self, data):
-        pass
-
-    def recv(self):
-        self.check_started()
-        (data, addr) = self._socket.recvfrom(1024)
-        process_data(data)
-
-    def __init__(self, port=37896):
-        self._port = port
 
 
 
